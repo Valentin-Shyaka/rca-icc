b21eba299b6e98cdc65090137042057b8bab0820
Mutesa-Cedric <mutesacedric@gmail.com>
2022-02-04 14:42:12 +0200

    update : schemas

--- src/cms/schemas/documents/trending.js
+++ src/cms/schemas/documents/trending.js
@@ -23,14 +23,23 @@ export default {
             description: "the image for the trending",
         },
         {
-            name: "text",
-            title: "Text",
-            type: "text",
+            name: "title",
+            title: "Title",
+            type: "string",
             description: "the title of the trending",
+            validation: Rule => Rule.required().min(3).max(100).warning(
+                "please provide a title for the trending"
+            )
+        },
+        {
+            name: "description",
+            title: "description",
+            type: "text",
+            description: "the description of the trending",
             validation: Rule => Rule.required().min(3).max(300).warning(
                 "please provide a title for the trending"
             )
-        }
+        },
     ]
 
 }
