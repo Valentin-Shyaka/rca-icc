ffe12e22f0aee863f69054f419f502ebf19687c1
Valentin-Shyaka <vava1lantern7@gmail.com>
2023-01-20 10:19:38 +0200

    added the matchCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import LiveGameCard from '../components/LiveGameCard'
 import CompNavBar from '../components/constants/CompNavBar'
 import SideBar from '../components/constants/SideBar'
 import MainLayout from '../layouts/MainLayout'
+import MatchCard from '../components/MatchCard'
 
 const Home: NextPage = () => {
   return (
 		<MainLayout title='Football'>
 			<main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
-				<LiveGameCard/>
+				{/* <LiveGameCard/> */}
+				<MatchCard/>
 			</main>
 		</MainLayout>
 	);
