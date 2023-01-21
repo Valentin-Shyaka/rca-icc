1c73d648e59bdfd236c7bf29494eaefc89a7f13b
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-01-21 12:35:36 +0200

    added stats and table

---
+++ src/pages/football/players.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../layouts/MainLayout'
+
+const PlayersIndex = () => {
+  return (
+		<MainLayout>
+			<div>PlayersIndex</div>
+		</MainLayout>
+	);
+}
+
+export default PlayersIndex
---
+++ src/pages/football/results.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../../layouts/MainLayout'
+
+const ResultsIndex = () => {
+  return (
+		<MainLayout>
+			<div>ResultsIndex</div>
+		</MainLayout>
+	);
+}
+
+export default ResultsIndex
