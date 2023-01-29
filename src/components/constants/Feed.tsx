b9032c8aff53ec6922150908c6dc193ab0846480
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-01-29 09:01:14 +0200

    updated insights

--- src/components/constants/Feed.tsx
+++ src/components/constants/Feed.tsx
@@ -7,13 +7,15 @@ const Feed = () => {
 			<h1 className=' text-center font-semibold'>Insights</h1>
 			<div className='w-[250px] float-right  border-2 border-gray h-[250px] rounded-md relative top-0 aspect-square'>
 				<Image
-					src={"/images/feedImage.png"}
+					src={"https://cdn.sanity.io/images/lxeru4rg/production/affa58f141f37bf3e08e91821c235f9742977b06-2104x2630.jpg"}
 					alt={"match feed"}
+					className=" aspect-video object-cover h-full"
 					width={250}
 					height={50}
 				/>
+				<h1 className="font-semibold text-center">Hattrick Hero</h1>
 				<p className='font-md font-sans text-coolGray-500 text-left p-2 text-sm'>
-					The so-called year 1 kids surprise their opponents with a draw at their first official match and Welding dissapoints.
+					Ishimwe Anglebert (Makaveli) becomes the first player to score a hat-trick in the history of the Rca Interclass competition.
 				</p>
 			</div>
 		</div>
