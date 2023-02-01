a30fa380b748acbc10f715b006b2a12e202841a5
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-02-01 08:33:00 +0200

    feat: friendly matches and enhanced trendings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,44 @@ const Home: NextPage = () => {
 	return (
 		<MainLayout title='ICC - Home' isGeneral>
 			<main className='flex w-full flex-1 flex-col p-2 gap-y-3 overflow-x-hidden'>
-				{friendlyMatches!?.length > 0 &&<div className='flex flex-col border-2 rounded-md p-2 border-gray'>
-					<h1 className='text-lg font-semibold'>Friendlies</h1>
-				</div>}
+				{trends!?.length > 0 && (
+					<div className='flex flex-col border-2 rounded-md p-2 border-gray gap-y-3'>
+						<h1 className='text-xl font-semibold'>Trendings</h1>
+						{trends?.map((trend) => (
+							<div key={trend._id} className='flex flex-col gap-y-3 w-full'>
+								<h1 className='text-lg font-semibold'>{trend.title}</h1>
+								<div className='flex md:flex-row flex-col w-full gap-3'>
+									<Image
+										className=' w-full object-cover md:w-1/2 aspect-video'
+										src={trend.image}
+										alt={trend.title}
+										width={1920}
+										height={1080}
+									/>
+									<div className='flex flex-col md:w-1/2 w-full'>
+										<p className=''>
+											{trend.description.slice(0, 200) + "..."}
+										</p>
+										<Link
+											href={`/trends/${trend._id}`}
+											className='w-fit mt-4 px-3 py-2 text-blue flex items-center hover:text-[#1a44da] duration-300 rounded-md'
+										>
+											Read More<span className='ml-2 mt-1'>{">>"}</span>
+										</Link>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
+				{friendlyMatches!?.length > 0 && (
+					<div className='flex flex-col border-2 rounded-md p-2 border-gray'>
+						<h1 className='text-lg font-semibold'>Friendlies</h1>
+						{friendlyMatches?.map((match, i) => (
+							<MatchCard key={match._id} {...match} />
+						))}
+					</div>
+				)}
 				<div className='flex flex-col border-2 rounded-md p-2 border-gray'>
 					<h1 className='text-xl font-semibold'>Latest Results</h1>
 					<div className='flex w-full mt-4 flex-wrap gap-3'>
@@ -45,32 +80,6 @@ const Home: NextPage = () => {
 					>
 						See All Fixtures<span className='ml-2 mt-1'>{">>"}</span>
 					</Link>
-				</div>
-				<div className='flex flex-col border-2 rounded-md p-2 border-gray gap-y-3'>
-					<h1 className='text-xl font-semibold'>Trendings</h1>
-					{trends?.map((trend) => (
-						<div key={trend._id} className='flex flex-col gap-y-3 w-full'>
-							<h1 className='text-lg font-semibold'>{trend.title}</h1>
-							<div className='flex md:flex-row flex-col w-full gap-3'>
-								<Image
-									className=' w-full object-cover md:w-1/2'
-									src={trend.image}
-									alt={trend.title}
-									width={1920}
-									height={1080}
-								/>
-								<div className='flex flex-col md:w-1/2 w-full'>
-									<p className=''>{trend.description.slice(0, 200) + "..."}</p>
-									<Link
-										href={`/trends/${trend._id}`}
-										className='w-fit mt-4 px-3 py-2 text-blue flex items-center hover:text-[#1a44da] duration-300 rounded-md'
-									>
-										Read More<span className='ml-2 mt-1'>{">>"}</span>
-									</Link>
-								</div>
-							</div>
-						</div>
-					))}
 				</div>
 				<div className='flex flex-col border-2 rounded-md p-2 border-gray'>
 					<h1 className='text-xl font-semibold'>Competitions</h1>
